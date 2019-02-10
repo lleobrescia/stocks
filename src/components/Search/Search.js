338823190f5a3ad0482f04b837a288f1858ba3b6
Leo Brescia <leonardo@leobrescia.com.br>
2019-02-10 13:08:28 -0200

    fix: Remove unnecessary import

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import AsyncSelect from 'react-select/lib/Async';
 import { connect } from 'react-redux';
